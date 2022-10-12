89f994b2c2f707b9dd9c9cb760ca6cc7f957c98b
Casper Andersson <casper.casan@gmail.com>
2022-10-12 17:41:36 +0200

    LICENSE: fix license headers, pick GPLv2
    The copied code had the option of picking between LGPLv2/GPLv2 and BSD 2/3 in
    many cases. In the case of OR options, choose LGPLv2/GPLv2. Upgrade
    LGPLv2 license to GPLv2 since that is allowed.
    
    Project now fully GPLv2.
    
    Signed-off-by: Casper Andersson <casper.casan@gmail.com>

--- kprobe/bootstrap.bpf.c
+++ kprobe/bootstrap.bpf.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
+// SPDX-License-Identifier: GPL-2.0-only
 /* Copyright (c) 2020 Facebook */
 #include "vmlinux.h"
 #include "bpf/bpf_helpers.h"
--- kprobe/tools/gen_vmlinux_h.sh
+++ kprobe/tools/gen_vmlinux_h.sh
@@ -1,3 +1,5 @@
 #/bin/sh
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) 2020 Facebook
 
 $(dirname "$0")/bpftool btf dump file ${1:-/sys/kernel/btf/vmlinux} format c
