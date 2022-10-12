e4453c38bd9b4dfa546fb1e6d700bb995040a3e6
Casper Andersson <casper.casan@gmail.com>
2022-10-12 17:41:36 +0200

    LICENSE: fix license headers, pick GPLv2
    The copied code had the option of picking between LGPLv2/GPLv2 and BSD 2/3 in
    many cases. In the case of OR options, choose LGPLv2/GPLv2. Upgrade
    LGPLv2 license to GPLv2 since that is allowed.
    
    Project now fully GPLv2.
    
    Signed-off-by: Casper Andersson <casper.casan@gmail.com>

--- kprobe/tools/cmake/FindBpfObject.cmake
+++ kprobe/tools/cmake/FindBpfObject.cmake
@@ -1,4 +1,5 @@
-# SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
+# SPDX-License-Identifier: GPL-2.0-only
+# SPDX-FileCopyrightText: 2021 Daniel Xu <dxu@dxuuu.xyz>
 
 #[=======================================================================[.rst:
 FindBpfObject
