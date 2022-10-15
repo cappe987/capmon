6878d2215ab29438af855904cad5040b2846f57a
Casper Andersson <casper.casan@gmail.com>
2022-10-15 11:33:10 +0200

    include: change guards to be uniform
    _CAPMON_filename_H_ format
    
    Signed-off-by: Casper Andersson <casper.casan@gmail.com>

--- include/monitor.h
+++ include/monitor.h
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-only
 // SPDX-FileCopyrightText: 2022 Casper Andersson <casper.casan@gmail.com>
 
-#ifndef _CAPMON_MONITOR_H
-#define _CAPMON_MONITOR_H
+#ifndef _CAPMON_MONITOR_H_
+#define _CAPMON_MONITOR_H_
 
 #include <stdbool.h>
 
@@ -12,4 +12,4 @@
 
 int run_monitor_mode(struct capmon *cm);
 
-#endif /* _CAPMON_MONITOR_H */
+#endif /* _CAPMON_MONITOR_H_ */
